1e82b2e6789951bc788ab1ff11349f82c84020e3
Rune H. Steinnes-Westum <rune.steinnes@westum.no>
2025-07-08 14:51:54 +0200

    First import

---
+++ README.md
@@ -0,0 +1,45 @@
+# tmuxsessions
+
+A simple command line interface for saving and loading tmux sessions.
+
+## Installation
+
+```bash
+python -m pip install --user 'git+https://github.com/roderik333/tmuxsessions.git#egg=tmuxsessions'
+```
+
+This will install a command `t` in your `~/.local/bin` directory.
+
+## Usage
+
+### Save Sessions
+
+To save the current tmux sessions to a file, run:
+
+```bash
+t save-sessions
+```
+
+This will save the sessions to `~/.tmux_sessions.json`.
+
+### Load Sessions
+
+To load tmux sessions from a file, run:
+
+```bash
+t load-sessions
+```
+
+This will restore sessions that have been closed but does not restore single windows inside existing sessions.
+
+## Aliases
+
+Useful aliases for `.zshrc`:
+
+```bash
+tm='tmux new -s '
+tma='tmux attach-session -t '
+tmd='tmux detach'
+tml='tmux list-sessions'
+tmc='tmux choose-tree'
+```
---
+++ src/tmuxsessions/main.py
@@ -0,0 +1,101 @@
+"""A simple command line interface for saving sessions.
+
+Based off of a bash script found on the internets.
+
+I prefer to have these aliases in my .zshrc file:
+tm='tmux new -s '
+tma='tmux attach-session -t '
+tmd='tmux detach'
+tml='tmux list-sessions'
+
+"""
+
+import json
+import os
+import pathlib
+import subprocess
+from contextlib import suppress
+from typing import TypeAlias, TypedDict, cast
+
+import click
+
+
+class Window(TypedDict):
+    window_name: str
+    path: str
+
+
+Session: TypeAlias = dict[str, list[Window]]
+SESSION_FILE = pathlib.Path.home() / ".tmux_sessions.json"
+
+
+def execute_command(command: str) -> str | None:
+    try:
+        result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        click.secho(f"Error executing command: {e}", err=True, fg="red")
+        return None
+
+
+@click.command()
+def save_sessions():
+    """A simple way to save sessions.
+
+    I don't use split windows so I wil not bother with implementing that.
+
+    """
+    command = "tmux list-windows -a -F '#S${d}#W${d}#{pane_current_path}'"
+    output = execute_command(command)
+    if output:
+        sessions: Session = {}
+        for line in output.splitlines():
+            # Kept this since the original bash script used it.
+            session_name, window_name, path = line.split("${d}")
+            if session_name not in sessions:
+                sessions[session_name] = []
+            sessions[session_name].append({"window_name": window_name, "path": path})
+        with open(SESSION_FILE, "w") as f:
+            json.dump(sessions, f, indent=4)
+        click.secho(f"Sessions saved to {SESSION_FILE}", fg="blue")
+
+
+@click.command()
+def load_sessions():
+    """A simple way to load sessions.
+
+    This will restore sessions that has been closed, but it will not resore single
+    windows inside existings sessions.
+    """
+    existing_sessions: str | None = None
+
+    if not os.path.exists(SESSION_FILE):
+        click.echo(f"Session file {SESSION_FILE} not found.", err=True)
+        return
+
+    with open(SESSION_FILE, "r") as f:
+        sessions: Session = cast("Session", json.load(f))
+
+    with suppress(subprocess.CalledProcessError):
+        existing_sessions = execute_command("tmux list-session -F '#S'")
+
+    for session_name, windows in sessions.items():
+        if existing_sessions is not None and session_name in existing_sessions:
+            continue
+        _ = execute_command(f"tmux new-session -d -s {session_name}")
+        for window in windows:
+            _ = execute_command(f"tmux new-window -t {session_name} -n {window['window_name']} -c {window['path']}")
+
+    click.secho("Sessions restored.", fg="green")
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(save_sessions)
+cli.add_command(load_sessions)
+
+if __name__ == "__main__":
+    cli()
